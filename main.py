583e2621578719fb79e4ca203e63a3720319325e
Manasye Shousen Bukit <32813070+manasye@users.noreply.github.com>
2018-01-19 08:07:56 +0700

    add spaces

--- main.py
+++ main.py
@@ -51,12 +51,45 @@ def printMatrix(matrix):
             print(matrix[x][y],end = '')
         print('')
 
-#def solveMatrix(matrix):
+def accessSpaces(matrix):
+    spaces = []
+    counter = 0
+    # access vertically
+    for j in range(0,len(matrix)):
+        i = 0
+        while (i < len(matrix)-1):
+            if(matrix[j][i] == '-') and (matrix[j][i+1] == '-'):
+                spaces.append([])
+                start = [j,i]
+                print(start)
 
+                a = i
+                length = 1
+
+                while (matrix[j][a+1] == '-') and (a+1 < len(matrix) - 1) :
+                    length = length + 1
+                    a = a + 1
+
+                finish = [j,a+1]
+                print(finish)
+                print(length)
+                i = i + length
+
+                spaces[counter].append(start)
+                spaces[counter].append(finish)
+                spaces[counter].append(length)
+                counter = counter + 1
+            else :
+                i = i + 1
+    print(spaces)
+    print(len(spaces))
+#def solveMatrix(matrix,words):
 
 if __name__ == '__main__':
-    myMatrix = accessMap('tc-5.txt')
-    myWords = accessWord('tc-5.txt')
+    myMatrix = accessMap('tc-0.txt')
+    myWords = accessWord('tc-0.txt')
     printMatrix(myMatrix)
-    print(len(myWords))
-    print(myWords)
+    #print(myMatrix[0][3])
+    accessSpaces(myMatrix)
+    #print(len(myWords))
+    #print(myWords)
